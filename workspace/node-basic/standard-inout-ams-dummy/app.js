59dfa6d92b57a321b8af656dab66bf321e24e6e2
Yunho Nam <101496294+ManDuGuk@users.noreply.github.com>
2024-08-09 17:02:37 +0900

    20240809

--- workspace/node-basic/standard-inout-ams-dummy/app.js
+++ workspace/node-basic/standard-inout-ams-dummy/app.js
@@ -4,31 +4,103 @@ const { menu,
     consoleInterface,
     readLine,
     parsJson,
+    parsJson2,
     allCounts } = require('./init');
 
+//랜덤 더미데이터 생성
 
+// 랜덤인트
+function getRandomInt() {
+    //0~9까지 나옴
+    return Math.floor(Math.random() * 10);
+};
+
+//랜덤네임(10개,동명이인허용)
+function getRandomName() {
+    const name = ["남윤호", "서윤호", "동윤호", "북윤호", "최윤호", "김윤호", "극윤호", "느그윤호", "부산윤호", "경기윤호"];
+    return name[getRandomInt()];
+}
+
+//랜던비번(4자리)
+function getRandomPassword() {
+    //0~9까지 나옴
+    return Math.floor(Math.random() * 10000);
+};
+
+//랜덤잔액(4자리)
+function getRandomBanlace() {
+    //0~9까지 나옴
+    return Math.floor(Math.random() * 10000);
+};
+
+
+
+
+//생성된 계좌를 저장한다. (중복된 계좌는 저장안됨)
+const generatedAccounts = new Set();
+
+// //랜덤계좌(중복고려?)
+// function getRandomNumber() {
+
+//     let accountnum;
+//     do {
+//         accountnum = `${Math.floor(Math.random() * 1000)}-${Math.floor(Math.random() * 1000)}`;
+//     } while (generatedAccounts.has(accountnum));
+
+//     generatedAccounts.add(accountnum);
+
+//     return generatedAccounts;
+// }
+
+function getRandomNumber() {
+    return `${Math.floor(Math.random() * 1000)}-${Math.floor(Math.random() * 1000)}`;
+}
+
+//랜덤 데이터 생성
+function makedummy(num) {
+    const dummy = [];
+    for (i = 0; i < num; i++) {
+        const account = {
+            _name: getRandomName(),
+            _password: getRandomPassword(),
+            _balance: getRandomBanlace(),
+            _number: getRandomNumber(),
+        }
+        dummy.push(account);
+    }
+    return dummy;
+}
+
+const dummyJson = JSON.stringify(Array.from(makedummy(100)))
+
+//더미 데이터 제이슨 생성
+modules.fs.writeFile('./dummy.json', dummyJson)
+    .then(() => console.log("파일 입력완료"))
+    .catch((e) => console.log(e.message));
+
+console.log("더미생성 완료");
 
 
 const main = (async function () {
     //시작메뉴 출력
     menu.start();
 
+    //입력이벤트에 따라서?
     while (true) {
-
-        //제이슨 파일 담을변수
-        let toJson = null;
-        let account = null;
 
         //메뉴 출력
         menu.category();
 
-
         let menuNum = parseInt(await readLine("> "));
+        let toJson = null;
 
         switch (menuNum) {
             case 1:
                 //
                 menu.selectAccount();
+
+                let account = null;
+
                 //입력대기
                 let no = parseInt(await readLine("> "));
                 let accountNum = await readLine("- 계좌번호 : ");
