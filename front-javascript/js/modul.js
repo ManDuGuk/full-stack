6c2d28befb2f36d017e911b8dbc9736470e1abf7
Yunho Nam <101496294+ManDuGuk@users.noreply.github.com>
2024-08-19 17:51:33 +0900

    20240819

---
+++ front-javascript/js/modul.js
@@ -0,0 +1,26 @@
+// export const MAX_VALUE = 100;
+// export const fruits = ["사과", "포토", "딸기"];
+// export const student = {
+//     ssn: 10,
+//     name: "남윤호",
+
+// };
+// export const sum = function (x, y) {
+//     return x + y;
+// }
+
+const MAX_VALUE = 100;
+const fruits = ["사과", "포토", "딸기"];
+const student = {
+    ssn: 10,
+    name: "남윤호",
+
+};
+const sum = function (x, y) {
+    return x + y;
+}
+
+//한방에 객체에 담아서 내보내기
+export {
+    MAX_VALUE, fruits, student, sum
+}
---
+++ front-javascript/js/student.js
@@ -0,0 +1,11 @@
+// export default function Student(name) {
+//     this.name = name;
+// }
+
+//함수 표현식은 이름앞에 export를 붙여서 내보내기가 잘 안된다. 
+
+function Student(name) {
+    this.name = name;
+}
+
+export default Student;
